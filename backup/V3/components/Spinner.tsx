775964d08433c01b362e43bb0889f356c6427b6a
saykim <sangyeony+github@gmail.com>
2025-08-22 19:37:06 +0900

    보안 강화 전 현재 상태 백업 - 프롬프트 노출 이슈 해결 전

--- backup/V3/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const Spinner: React.FC = () => {
-  return (
-    <div className="w-12 h-12 border-4 border-blue-200 border-t-blue-500 border-solid rounded-full animate-spin"></div>
-  );
-};
